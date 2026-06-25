96c1fe0c2972d95e7ece5ec2039a1afa41c7e554
bsa1987 <bruno_santos1987@hotmail.com>
2026-06-25 10:45:58 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,8 @@
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Services from './components/Services';
-import Gallery from './components/Gallery';
-import About from './components/About';
-import Reviews from './components/Reviews';
-import AppointmentForm from './components/AppointmentForm';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-
 export default function App() {
   return (
-    <>
-      <Navbar />
-      <main>
-        <Hero />
-        <Services />
-        <Gallery />
-        <About />
-        <Reviews />
-        <AppointmentForm />
-        <Contact />
-      </main>
-      <Footer />
-    </>
+    <div style={{ padding: '40px' }}>
+      <h1>Test</h1>
+      <p>If you can see this, React is working.</p>
+    </div>
   );
 }
