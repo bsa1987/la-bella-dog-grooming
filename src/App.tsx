aeeb73f3978a312358db0268297c89fae42745bc
bsa1987 <bruno_santos1987@hotmail.com>
2026-06-26 18:28:26 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Hero from './components/Hero';
 import Services from './components/Services';
+import Gallery from './components/Gallery';
 
 export default function App() {
   return (
     <>
       <Hero />
       <Services />
+      <Gallery />
     </>
   );
 }
