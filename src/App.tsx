c6093ff9f45912c09b52ecca3150da7c3fd3207a
bsa1987 <bruno_santos1987@hotmail.com>
2026-06-26 18:10:32 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
+import Navbar from './components/Navbar';
 
 export default function App() {
   return (
-    <div style={{ padding: '40px' }}>
-      <h1>Test</h1>
-      <p>If you can see this, React is working.</p>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 }
