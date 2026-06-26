9a0b26e57ee2052f4e7c5bbf1f9452796d787b6a
bsa1987 <bruno_santos1987@hotmail.com>
2026-06-26 19:05:27 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import About from './components/About';
 import Reviews from './components/Reviews';
 import AppointmentForm from './components/AppointmentForm';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 export default function App() {
   return (
@@ -16,6 +17,7 @@ export default function App() {
       <Reviews />
       <AppointmentForm />
       <Contact />
+      <Footer />
     </>
   );
 }
