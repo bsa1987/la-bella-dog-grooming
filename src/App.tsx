00cbe1869b2b973cafc3428c971cf826d3e15df8
bsa1987 <bruno_santos1987@hotmail.com>
2026-06-26 18:22:56 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
-import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 
 export default function App() {
-  return (
-    <>
-      <Navbar />
-    </>
-  );
+  return <Hero />;
 }
